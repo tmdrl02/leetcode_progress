d3153f242fb8e2b60293a7a65058f2c147de2037
Seunggi Hong <seulggi02@gmail.com>
2025-06-11 16:38:57 -0400

    Fixed to show the exact number of problems on Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { Link, useResolvedPath } from "react-router-dom";
 import { ShoppingBagIcon, ShoppingCartIcon, FileCode } from "lucide-react";
 import ThemeSelector from "./ThemeSelector";
-// import { useProductStore } from "../store/useProductStore";
+import { useProblemStore } from "../store/useProblemStore";
 
 function Navbar() {
   const { pathname } = useResolvedPath();
   const isHomePage = pathname === "/";
 
-//  const { products } = useProductStore();
+  const { problems } = useProblemStore();
+
 
   return (
     <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
@@ -36,7 +37,9 @@ function Navbar() {
               <div className="indicator">
                 <div className="p-2 rounded-full hover:bg-base-200 transition-colors">
                   <ShoppingBagIcon className="size-5" />
-                  <span className="badge badge-sm badge-primary indicator-item">8</span>
+                  <span className="badge badge-sm badge-primary indicator-item">
+                    {problems.length}
+                  </span>
                 </div>
               </div>
             )}
